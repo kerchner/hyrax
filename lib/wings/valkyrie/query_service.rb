8170e6678bf7b0472fa5904f97f0eb631ed44601
tamsin johnson <tomjohnson@ucsb.edu>
2021-03-18 04:58:57 -0700

    add support for 'count all of model`
    reduce failing tests for Valkyrie queries to 11.
    
    related to #3570

--- lib/wings/valkyrie/query_service.rb
+++ lib/wings/valkyrie/query_service.rb
@@ -7,11 +7,24 @@ module Wings
       extend Forwardable
       def_delegator :adapter, :resource_factory
 
-      # @param adapter [Wings::Valkyrie::MetadataAdapter] The adapter which holds the resource_factory for this query_service.
+      ##
+      # @param adapter [Wings::Valkyrie::MetadataAdapter] The adapter which
+      #   holds the resource_factory for this query_service.
       def initialize(adapter:)
         @adapter = adapter
       end
 
+      ##
+      # @param :model [Class]
+      #
+      # @return [Integer]
+      def count_all_of_model(model:)
+        ActiveFedora::Base
+          .where(has_model_ssim: [model_class_for(model).to_s, model.new.internal_resource.to_s])
+          .count
+      end
+
+      ##
       # WARNING: In general, prefer find_by_alternate_identifier over this
       # method.
       #
@@ -23,14 +36,17 @@ module Wings
       # start getting ObjectNotFoundErrors instead of the objects you wanted
       #
       # Find a record using a Valkyrie ID, and map it to a Valkyrie Resource
+      #
       # @param [Valkyrie::ID, String] id
       # @return [Valkyrie::Resource]
-      # @raise [Hyrax::ObjectNotFoundError]
+      # @raise [Valkyrie::Persistence::ObjectNotFoundError]
       def find_by(id:)
         find_by_alternate_identifier(alternate_identifier: id)
       end
 
+      ##
       # Find all work/collection records, and map to Valkyrie Resources
+      #
       # @return [Array<Valkyrie::Resource>]
       def find_all
         ::ActiveFedora::Base.all.map do |obj|
@@ -38,7 +54,10 @@ module Wings
         end
       end
 
-      # Find all work/collection records of a given model, and map to Valkyrie Resources
+      ##
+      # Find all work/collection records of a given model, and map to Valkyrie
+      # Resources
+      #
       # @param model [Class]
       # @return [Array<Valkyrie::Resource>]
       def find_all_of_model(model:)
@@ -47,7 +66,10 @@ module Wings
         end
       end
 
-      # Find an array of record using Valkyrie IDs, and map them to Valkyrie Resources maintaining order based on given ids
+      ##
+      # Find an array of record using Valkyrie IDs, and map them to Valkyrie
+      # Resources maintaining order based on given ids
+      #
       # @param [Array<Valkyrie::ID, String>] ids
       # @return [Array<Valkyrie::Resource>]
       # NOTE: Ignores non-existent ids.
@@ -68,24 +90,32 @@ module Wings
         resources
       end
 
+      ##
       # Find a record using an alternate ID, and map it to a Valkyrie Resource
+      #
       # @param [Valkyrie::ID, String] id
       # @param [boolean] optionally return ActiveFedora object/errors
+      #
       # @return [Valkyrie::Resource]
-      # @raise [Hyrax::ObjectNotFoundError]
+      # @raise [Valkyrie::Persistence::ObjectNotFoundError]
       def find_by_alternate_identifier(alternate_identifier:, use_valkyrie: true)
-        alternate_identifier = ::Valkyrie::ID.new(alternate_identifier.to_s) if alternate_identifier.is_a?(String)
+        alternate_identifier = ::Valkyrie::ID.new(alternate_identifier.to_s) if
+          alternate_identifier.is_a?(String)
         validate_id(alternate_identifier)
         object = ::ActiveFedora::Base.find(alternate_identifier.to_s)
         return object if use_valkyrie == false
         resource_factory.to_resource(object: object)
-      rescue ActiveFedora::ObjectNotFoundError, Ldp::Gone
-        raise Hyrax::ObjectNotFoundError
+      rescue ActiveFedora::ObjectNotFoundError, Ldp::Gone => err
+        raise err unless use_valkyrie
+        raise ::Valkyrie::Persistence::ObjectNotFoundError
       end
 
+      ##
       # Find all members of a given resource, and map to Valkyrie Resources
+      #
       # @param resource [Valkyrie::Resource]
       # @param model [Class]
+      #
       # @return [Array<Valkyrie::Resource>]
       def find_members(resource:, model: nil)
         return [] unless resource.respond_to?(:member_ids) && resource.member_ids.present?
@@ -95,21 +125,30 @@ module Wings
         all_members.select { |member_resource| model_class_for(member_resource.class) == find_model }
       end
 
+      ##
       # Find the Valkyrie Resources referenced by another Valkyrie Resource
+      #
       # @param resource [<Valkyrie::Resource>]
       # @param property [Symbol] the property holding the references to another resource
       # @return [Array<Valkyrie::Resource>]
-      def find_references_by(resource:, property:)
+      def find_references_by(resource:, property:, model: nil)
+        return find_many_by_ids(ids: Array(resource.send(property))) unless model
+
         object = resource_factory.from_resource(resource: resource)
-        object.send(property).map do |reference|
+
+        results = object.send(property).map do |reference|
           af_id = find_id_for(reference)
           resource_factory.to_resource(object: ::ActiveFedora::Base.find(af_id))
         end
+
+        results.select { |r| r.class.name == model.name }
       rescue ActiveFedora::ObjectNotFoundError
         []
       end
 
+      ##
       # Get all resources which link to a resource or id with a given property.
+      #
       # @param resource [Valkyrie::Resource] The resource which is being referenced by
       #   other resources.
       # @param resource [Valkyrie::ID] The id of the resource which is being referenced by
@@ -123,7 +162,7 @@ module Wings
       #   in order.
       def find_inverse_references_by(resource: nil, id: nil, property:)
         raise ArgumentError, "Provide resource or id" unless resource || id
-        id ||= resource.alternate_ids.first
+        id ||= resource.id
         raise ArgumentError, "Resource has no id; is it persisted?" unless id
         uri = Hyrax::Base.id_to_uri(id.to_s)
         ActiveFedora::Base.where("+(#{property}_ssim: \"#{uri}\" OR #{property}_ssim: \"#{id}\")").map do |obj|
@@ -131,20 +170,24 @@ module Wings
         end
       end
 
+      ##
       # Find all parents of a given resource.
+      #
       # @param resource [Valkyrie::Resource] The resource whose parents are being searched
       #   for.
       # @return [Array<Valkyrie::Resource>] All resources which are parents of the given
       #   `resource`. This means the resource's `id` appears in their `member_ids`
       #   array.
       def find_parents(resource:)
-        id = resource.alternate_ids.first
-        ActiveFedora::Base.where("member_ids_ssim: \"#{id}\"").map do |obj|
+        ActiveFedora::Base.where("member_ids_ssim: \"#{resource.id}\"").map do |obj|
           resource_factory.to_resource(object: obj)
         end
       end
 
-      # Constructs a Valkyrie::Persistence::CustomQueryContainer using this query service
+      ##
+      # Constructs a Valkyrie::Persistence::CustomQueryContainer using this
+      # query service
+      #
       # @return [Valkyrie::Persistence::CustomQueryContainer]
       def custom_queries
         @custom_queries ||= ::Valkyrie::Persistence::CustomQueryContainer.new(query_service: self)
@@ -152,7 +195,9 @@ module Wings
 
       private
 
+      ##
       # Determines whether or not an Object is a Valkyrie ID
+      #
       # @param [Object] id
       # @raise [ArgumentError]
       def validate_id(id)
